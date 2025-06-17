5c287aa03db91e6f92f986c1c07dff68067506a2
matrakei <163850982+matrakei@users.noreply.github.com>
2025-06-17 10:40:05 -0300

    parte del header hecho

--- zwap/src/components/Footer/Footer.jsx
+++ zwap/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css' ;
 export const Footer = () => {
   return (
     <footer className='footer'>
-    <h1 className='subtitulo'>Footer</h1>
+    <h1 className='subtitulo'>text</h1>
     </footer>
   )
 }
--- zwap/src/components/Header/Header.jsx
+++ zwap/src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
-import './Header.css' ;
+import './Header.css';
 
 export const Header = () => {
-    return (
-        <header className="header"> 
-            <h1 className="titulo">Header</h1>
-        </header>
-    )
+  return (
+    <header className="header">
+      <h1 className="titulo">Zwap</h1>
+      
+      <div className="buttons-right">
+        <button className="home-button">Home</button>
+        <button className="perfil-button">Nombre y apellido</button>
+      </div>
+    </header>
+  );
 }
