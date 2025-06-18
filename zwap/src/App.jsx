5ab06e0afadcb5d177b0d9cf234f3aef9bf8b3b6
matrakei <163850982+matrakei@users.noreply.github.com>
2025-06-18 12:00:38 -0300

    section creado

--- zwap/src/App.jsx
+++ zwap/src/App.jsx
@@ -1,14 +1,13 @@
-import { Header } from './components/Header/Header'
+import {Header} from './components/Header/Header'
 import {Footer} from './components/Footer/Footer'
+import {Section} from './components/Section/Section'
 import './App.css'
 
 function App() {
   return (
     <>
       <Header />
-      <section>
-        <p>este es el section</p>
-      </section>
+      <Section/>
       <Footer />
     </>
   )
---
+++ zwap/src/components/Section/Section.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Section = () => {
+  return (
+    <h1>este es el section</h1>
+  )
+}
+
+export default Section
