68dfa0dd07b024abdc2b7f37be360d8d9aa9febb
Matias Vernet <48519913@est.ort.edu.ar>
2025-07-02 12:01:08 -0300

    .

--- zwap/src/components/Step2/Step2.jsx
+++ zwap/src/components/Step2/Step2.jsx
@@ -25,14 +25,10 @@ export default function Step2() {
           </select>
         </label>
 
+        
         <label>
-          <select>
-            <option>Seleccioná el N° de metros cuadrados</option>
-            <option>20-40</option>
-            <option>40-60</option>
-            <option>60-80</option>
-            <option>+100</option>
-          </select>
+          Escribí el N° de metros cuadrados
+          <input type="text" placeholder="Type here..." />
         </label>
 
         <label>
